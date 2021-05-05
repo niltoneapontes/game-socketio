58a5f6abe68f00dcdc5b9be937bee6b7e7cad5ed
niltoneapontes <niltoneapontes@gmail.com>
2021-05-05 18:35:56 -0300

    feat: add player movement

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app); // cria o servidor baseado no express
+const io = socketio(server); // utiliza o servidor para criar a conexão client/server
+
+app.use(express.static(path.join(__dirname, 'public'))); // seta a pasta public como fonte do conteúdo "frontend"
+
+io.on('connection', socket => {
+  socket.emit('userJoin', socket.id);
+  socket.broadcast.emit('enemyJoin', socket.id);
+
+  socket.on('playerPosition', ({ id, x, y }) => {
+    socket.broadcast.emit('enemyPosition', { id, x, y })
+  })
+});
+
+server.listen(3000, () => console.log('Servidor rodando na porta 3000 ⚡️'))
---
+++ public/main.js
@@ -0,0 +1,35 @@
+const socket = io();
+
+socket.on('userJoin', (id) => {
+  document.getElementById('field').addEventListener('mousemove', (event) => {
+    document.getElementById('character').style.left = `${event.x}px`;
+    document.getElementById('character').style.top = `${event.y}px`;
+  
+    socket.emit('playerPosition', {
+      id: id,
+      x: event.x,
+      y: event.y
+    });
+  });
+
+  socket.on('enemyJoin', (enemyId) => {
+    const enemyPosition = document.createElement('div');
+    enemyPosition.classList.add('enemy');
+    enemyPosition.id = enemyId;
+    document.querySelector('.container').appendChild(enemyPosition);
+  });
+  
+  socket.on('enemyPosition', ({ id, x, y }) => {
+    const foundEnemy = document.getElementById(`${id}`);
+
+    if(!foundEnemy) {
+      const enemyPosition = document.createElement('div');
+      enemyPosition.classList.add('enemy');
+      enemyPosition.id = id;
+      document.querySelector('.container').appendChild(enemyPosition);
+    }
+
+    document.getElementById(`${id}`).style.left = `${x}px`;
+    document.getElementById(`${id}`).style.top = `${y}px`;
+  })
+})
