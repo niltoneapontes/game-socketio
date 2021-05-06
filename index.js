118998638fbe1496e6c67000466ebfa82345741f
niltoneapontes <niltoneapontes@gmail.com>
2021-05-05 22:05:39 -0300

    fix: port number

--- index.js
+++ index.js
@@ -18,4 +18,4 @@ io.on('connection', socket => {
   })
 });
 
-server.listen(3000 || process.env.PORT, () => console.log('Servidor rodando na porta 3000 ⚡️'));
+server.listen(process.env.PORT || 3000, () => console.log('Servidor rodando na porta 3000 ⚡️'));
